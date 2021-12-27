912b1e0cac7ac74c8c7dd4d7c168b5eb37ae58f8
Dalton Ma <85321730+dalton77-get@users.noreply.github.com>
2021-12-26 18:16:54 -0600

    Update imports other style issues

--- highlight/highlightapp/views.py
+++ highlight/highlightapp/views.py
@@ -1,15 +1,20 @@
+"""
+This file provides views for the Pygments Web App.
+"""
+
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import Http404, HttpResponse
-from pygments import highlight
-from pygments.lexers import *
-from pygments.formatters import HtmlFormatter
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
-from django.template.loader import render_to_string
-from weasyprint import HTML, CSS
-from weasyprint.text.fonts import FontConfiguration
-import tempfile
 from jinja2 import Environment, PackageLoader, select_autoescape
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
+from pygments.lexers.python import PythonLexer
+from pygments.lexers.c_cpp import CLexer, CppLexer
+from pygments.lexers.shell import BashLexer, BashSessionLexer, FishShellLexer
+from pygments.lexers.html import HtmlLexer
+from weasyprint import CSS, HTML
+from weasyprint.text.fonts import FontConfiguration
 
 env = Environment(
     loader=PackageLoader("highlightapp"),
@@ -24,10 +29,17 @@ rainbow += ".highlight { background: white; }"
 # Create your views here.
 
 def index(request):
+    """
+    Index route.
+    """
     return render(request, "highlight/index.html")
+
 
 @require_http_methods(["GET", "POST"])
 def highlightc(request, language=""):
+    """
+    This provides the editor interface and the syntax highlighter.
+    """
     if request.method == "POST":
         # Get code
         code = request.POST.get("code")
@@ -37,17 +49,23 @@ def highlightc(request, language=""):
         highlighter = get_lexer_by_name(language)
         formatter = HtmlFormatter(linenos=True)
         result = highlight(code, highlighter, formatter)
+        # Return highlighted code
         return render(request, "highlight/highlight.html", {
             "highlight": result
         })
     if not language:
         return HttpResponse(status=404)
+    # Return Editor Page
     return render(request, "highlight/edit.html", {
         "language": language
     })
 
+
 @require_http_methods(["GET", "POST"])
 def topdf(request, language=""):
+    """
+    This method puts the highlighted code into a pdf.
+    """
     font_config = FontConfiguration()
     if request.method == "POST":
         if not language:
@@ -63,18 +81,10 @@ def topdf(request, language=""):
         template = env.get_template("highlight/highlight.html")
         htmlresult = template.render(highlight=result)
         # Pdf
-        fontcss = """
-            pre {
-                font-family: 'Cascadia Code', sans-serif;
-            }
-            code {
-                font-family: 'Cascadia Code', sans-serif;
-            }
-        """
-        font = CSS(string=fontcss, font_config=font_config)
         css = CSS(string=rainbow, font_config=font_config)
         html = HTML(string=htmlresult)
-        pdfresult = html.write_pdf(presentational_hints=True, font_config=font_config, stylesheets=[css])
+        pdfresult = html.write_pdf(
+            presentational_hints=True, font_config=font_config, stylesheets=[css])
         # Create response headers
         response = HttpResponse(pdfresult, content_type='application/pdf;')
         response['Content-Disposition'] = 'inline; filename=code.pdf'
