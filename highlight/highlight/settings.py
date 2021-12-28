33e5fd3701e05584a8870f6c6568cbfd2d324862
Dalton Ma <85321730+dalton77-get@users.noreply.github.com>
2021-12-28 11:04:44 -0600

    Take off of debug mode

--- highlight/highlight/settings.py
+++ highlight/highlight/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@_c(71#u(x^sg)rki#h@5o=v*$2ix!h^4ss^ej_-msoph-albs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "*"]
 
 
 # Application definition
